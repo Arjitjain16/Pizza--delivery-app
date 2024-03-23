7a4e8605b29c6dde5f9ab93035f90d4893f2b9a7
Arjitjain16 <arjitvansh16@gmail.com>
2024-03-23 17:56:05 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Featured from './components/Featured';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container h-full w-full bg-red-600">
       <Navbar />
       <head>
       <title>My pizza app</title>
       <meta name='description' content='best pizza iun the town' />
       </head>
-      <div className="content bg-red-600 flex h-full w-full">
-        <div className="left"></div>
-        <div className="right"></div>
-      </div>
+      <Featured/>
       <Footer/>
     </div>
   );
---
+++ src/components/Featured.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Featured.css'; // Importing featured.css
+
+
+function Featured() {
+  return (
+    <div className="container-img overflow-x-hidden " style={{ overflow: 'hidden' }}>
+        <div className="img1 ">
+        <img src="/img/left arrow.png" className='img-left h-9' alt="" />
+            <div className="cont1 ">
+
+                <div className="section1 flex items-center flex-col">
+                    <h4 className='font-bold text-3xl'>Hot & Spicy</h4>
+                    <h1 className='font-bold text-9xl'>PIZZA</h1>
+                </div>
+                <div className="section2 flex items-center flex-col">
+                    <h1 className='font-bold text-3xl'>50% OFF</h1>
+
+                    <h1 className='font-bold text-3xl'>ORDER NOW</h1>
+                    <h3 className='font-bold text-2xl'>Brew Pizza</h3>
+                </div>
+            </div>
+            <div className="cont2">
+            <img src="/img/pngwing.com (9).png" alt="" />
+            </div>  
+            <img src="/img/right-arrow.png" className='img-right h-9' alt="" />
+        </div>
+        <div className="img2">
+            <img src="/img/PngItem_33910.png" alt="" />
+            
+        </div>
+        <div className="img3">
+            <img src="/img/mixed-pizza-with-various-ingridients-removebg-preview.png" alt="" />
+        
+        </div>
+        
+    </div>
+  );
+}
+
+export default Featured;
+
+
